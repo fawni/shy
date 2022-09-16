84e99dcd0c587321bafbcd23a3eaff476c65eb7a
obscurity <z@x4.pm>
2022-09-16 15:01:23 +0200

    refactor: use if instead of match

--- src/lib/command.rs
+++ src/lib/command.rs
@@ -94,46 +94,43 @@ async fn parse_position(input: impl ToString) -> Result<impl ToString, Box<dyn E
     let amount = input.to_string();
     let np = NowPlaying::new().await?;
     let (pos, total) = (np.position, np.duration);
-    match amount.ends_with('%') {
-        true => {
-            let percentage = amount.trim_end_matches('%');
-            // +5%
-            if percentage.starts_with('+') {
-                let amount = percentage.trim_start_matches('+').parse::<u32>()?;
-                let current = (pos / total) * 100;
-                let desired = current + amount;
-                let res = (desired * total) / 100;
-                Ok(res.to_string())
-            // -5%
-            } else if percentage.starts_with('-') {
-                let amount = percentage.trim_start_matches('-').parse::<u32>()?;
-                let current = (pos / total) * 100;
-                let desired = current - amount;
-                let res = (desired * total) / 100;
-                Ok(res.to_string())
-            // 5%
-            } else {
-                let amount = percentage.parse::<u32>()?;
-                let res = (total * amount) / 100;
-                Ok(res.to_string())
-            }
+    if amount.ends_with('%') {
+        let percentage = amount.trim_end_matches('%');
+        // +5%
+        if percentage.starts_with('+') {
+            let amount = percentage.trim_start_matches('+').parse::<u32>()?;
+            let current = (pos / total) * 100;
+            let desired = current + amount;
+            let res = (desired * total) / 100;
+            Ok(res.to_string())
+        // -5%
+        } else if percentage.starts_with('-') {
+            let amount = percentage.trim_start_matches('-').parse::<u32>()?;
+            let current = (pos / total) * 100;
+            let desired = current - amount;
+            let res = (desired * total) / 100;
+            Ok(res.to_string())
+        // 5%
+        } else {
+            let amount = percentage.parse::<u32>()?;
+            let res = (total * amount) / 100;
+            Ok(res.to_string())
         }
-        false => {
-            // +5 (seconds)
-            if amount.starts_with('+') {
-                let amount = amount.trim_start_matches('+').parse::<u32>()? * 1000;
-                let res = pos + amount;
-                Ok(res.to_string())
-            // -5 (seconds)
-            } else if amount.starts_with('-') {
-                let amount = amount.trim_start_matches('-').parse::<u32>()? * 1000;
-                let res = pos - amount;
-                Ok(res.to_string())
-            // 5 (will treat as 5% anyway)
-            } else {
-                let res = (total * amount.parse::<u32>()?) / 100;
-                Ok(res.to_string())
-            }
+    } else {
+        // +5 (seconds)
+        if amount.starts_with('+') {
+            let amount = amount.trim_start_matches('+').parse::<u32>()? * 1000;
+            let res = pos + amount;
+            Ok(res.to_string())
+        // -5 (seconds)
+        } else if amount.starts_with('-') {
+            let amount = amount.trim_start_matches('-').parse::<u32>()? * 1000;
+            let res = pos - amount;
+            Ok(res.to_string())
+        // 5 (will treat as 5% anyway)
+        } else {
+            let res = (total * amount.parse::<u32>()?) / 100;
+            Ok(res.to_string())
         }
     }
 }
