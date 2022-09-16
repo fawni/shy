5591ba6800ad9ca851a012efd25d5b9d4cbfe2c4
obscurity <z@x4.pm>
2022-09-16 19:12:03 +0200

    feat: add progress bar to nowplaying

--- src/lib/queue.rs
+++ src/lib/queue.rs
@@ -1,18 +1,16 @@
 use crate::{helper::parse_duration, NowPlaying};
 use owo_colors::OwoColorize;
-use std::error::Error;
+use std::{cmp::max, error::Error};
 
 pub async fn nowplaying() -> Result<String, Box<dyn Error>> {
     let np = NowPlaying::new().await?;
-    let current = np.position;
-    let total = np.duration;
-
+    let (pos, total) = (np.position, np.duration);
     let res = format!(
-        "{}\n{}\n{} / {}",
-        np.title.bold(),
-        np.artist.bright_black(),
-        parse_duration(current),
+        "{} {} {}\n{}",
+        parse_duration(pos),
+        pb(pos, total),
         parse_duration(total),
+        format_info(np),
     );
 
     Ok(res)
@@ -23,4 +21,41 @@ pub async fn volume() -> Result<String, Box<dyn Error>> {
     let volume = ((np.volume * 100.0) as u32).to_string();
 
     Ok(volume)
+}
+
+fn pb(pos: u32, total: u32) -> String {
+    const BAR_LENGTH: usize = 25;
+    let (c, t) = (pos as usize, total as usize);
+    let p = max((c * BAR_LENGTH) / t, 1);
+    let l = BAR_LENGTH - p;
+
+    format!("{}{}", "━".repeat(p).red(), "━".repeat(l).bright_black())
+}
+
+fn format_info(np: NowPlaying) -> String {
+    let t_h = np.duration / 1000 / 60 / 60;
+    let p_h = np.position / 1000 / 60 / 60;
+
+    // guhhhhhhhhhhhhhhh
+    if t_h > 0 {
+        if p_h > 0 {
+            format!(
+                "{:^43.43}\n{:^43.43}",
+                np.title.bold(),
+                np.artist.bright_black()
+            )
+        } else {
+            format!(
+                "{:^40.40}\n{:^40.40}",
+                np.title.bold(),
+                np.artist.bright_black()
+            )
+        }
+    } else {
+        format!(
+            "{:^37.37}\n{:^37.37}",
+            np.title.bold(),
+            np.artist.bright_black()
+        )
+    }
 }
